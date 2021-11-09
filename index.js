40a49f18073c2fbff99a5102836d68d2ed612094
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2021-11-09 16:08:14 +0200

    added upBtn&darkthm&some styles

--- index.js
+++ index.js
@@ -18,3 +18,38 @@ function scrollToTop() {
     behavior: 'smooth',
   });
 }//функция скрола вверх
+
+const toggleSwitch = document.querySelector('.theme-switcher input[type="checkbox"]');//ссылка на тугл
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};// место хранения тем
+
+toggleSwitch.addEventListener('change', switchTheme, false);//прослушка на переключателе тем
+
+ function switchTheme(e) {
+  if (e.target.checked) {//проверка положения тугла
+    document.documentElement.setAttribute('data-theme', Theme.DARK);
+    
+    document.querySelector('img').classList.add('imgboxDark');
+    document.querySelector('img').classList.remove('imgboxLight');
+    
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    document.documentElement.setAttribute('data-theme', Theme.LIGHT);
+    document.querySelector('img').classList.remove('imgboxDark');
+    document.querySelector('img').classList.add('imgboxLight');
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}// функция смены темы
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;// проверку текущей темы
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === Theme.DARK) {
+    toggleSwitch.checked = true;
+  }//проверка положения переключателя
+}
